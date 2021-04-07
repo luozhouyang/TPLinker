61f413000c2b5ce431fa0d57e47465ec00257b03
luozhouyang <zhouyang.luo@gmail.com>
2021-04-07 21:00:34 +0800

    Add GloveEmbedding test

--- tests/layers_torch_test.py
+++ tests/layers_torch_test.py
@@ -3,11 +3,24 @@ import unittest
 import numpy as np
 import torch
 from tplinker_pytorch.layers_torch import (ConcatHandshaking,
-                                           DistanceEmbedding, TaggingProjector,
-                                           TPLinker)
+                                           DistanceEmbedding, GloveEmbedding,
+                                           TaggingProjector, TPLinker)
 
 
 class LayersTest(unittest.TestCase):
+
+    def test_glove_embedding(self):
+        words = []
+        with open('data/bert-base-cased/vocab.txt', mode='rt', encoding='utf-8') as fin:
+            for line in fin:
+                words.append(line.rstrip('\n'))
+        vocab = {}
+        for idx, token in enumerate(words):
+            vocab[idx] = token
+        ge = GloveEmbedding('data/glove_300_nyt.emb', vocab=vocab, embedding_size=300)
+        input_ids = torch.tensor([[1, 2, 3, 4, 5, 6, 7, 8]], dtype=torch.int64)
+        output = ge(input_ids)
+        self.assertEqual([1, 8, 300], list(output.size()))
 
     def test_distance_embedding(self):
         de = DistanceEmbedding()
