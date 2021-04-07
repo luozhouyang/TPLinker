6da28fa4e3dca6ca0ffa5ea286397be2af5f0db2
luozhouyang <zhouyang.luo@gmail.com>
2021-04-07 19:12:08 +0800

    Add pytorch implementation

---
+++ tplinker_pytorch/layers_torch.py
@@ -0,0 +1,161 @@
+import logging
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+from glove import Glove
+
+
+class GloveEmbedding(nn.Module):
+
+    def __init__(self, pretrained_embedding_path, vocab, embedding_size, freeze=False, **kwargs):
+        super().__init__()
+        self.vocab = vocab
+        self.vocab_size = len(self.vocab)
+        self.embedding_size = embedding_size
+        self.glove = Glove.load(pretrained_embedding_path)
+        self.embedding = nn.Embedding.from_pretrained(self._build_embedding_matrix(), freeze=freeze)
+
+    def _build_embedding_matrix(self):
+        matrix = np.random.normal(-1, 1, size=(self.vocab_size, self.embedding_size))
+        count = 0
+        for idx, token in self.vocab.items():
+            if token in self.glove.dictionary:
+                matrix[idx] = self.glove.word_vectors(self.glove.dictionary[token])
+                count += 1
+        logging.info(f'Load {count} tokens from pretrained embedding table.')
+        return matrix
+
+    def forward(self, input_ids, **kwargs):
+        return self.embedding(input_ids)
+
+
+class DistanceEmbedding(nn.Module):
+
+    def __init__(self, max_positions=512, embedding_size=768, **kwargs):
+        super().__init__()
+        self.max_positions = max_positions
+        self.embedding_size = embedding_size
+        self.dist_embedding = self._init_embedding_table()
+        self.register_parameter('distance_embedding', self.dist_embedding)
+
+    def _init_embedding_table(self):
+        matrix = np.zeros([self.max_positions, self.embedding_size])
+        for d in range(self.max_positions):
+            for i in range(self.embedding_size):
+                if i % 2 == 0:
+                    matrix[d][i] = math.sin(d / 10000**(i / self.embedding_size))
+                else:
+                    matrix[d][i] = math.cos(d / 10000**((i - 1) / self.embedding_size))
+        embedding_table = nn.Parameter(data=torch.tensor(matrix, requires_grad=False), requires_grad=False)
+        return embedding_table
+
+    def forward(self, inputs, **kwargs):
+        """Distance embedding.
+
+        Args:
+            inputs: Tensor, shape (batch_size, seq_len, hidden_size)
+
+        Returns:
+            embedding: Tensor, shape (batch_size, 1+2+...+seq_len, embedding_size)
+        """
+        batch_size, seq_len = inputs.size()[0], inputs.size()[1]
+        segs = []
+        for index in range(seq_len, 0, -1):
+            segs.append(self.dist_embedding[:index, :])
+        segs = torch.cat(segs, dim=0)
+        embedding = segs[None, :, :].repeat(batch_size, 1, 1)
+        return embedding
+
+
+class TaggingProjector(nn.Module):
+
+    def __init__(self, hidden_size, num_relations, name='TaggingProjector', **kwargs):
+        super().__init__()
+        self.name = name
+        self.fc_layers = [nn.Linear(hidden_size, 3) for _ in range(num_relations)]
+        for index, fc in enumerate(self.fc_layers):
+            self.register_parameter('{}_weights_{}'.format(self.name, index), fc.weight)
+            self.register_parameter('{}_bias_{}'.format(self.name, index), fc.bias)
+
+    def forward(self, hidden, **kwargs):
+        """Project hiddens to tags for each relation.
+
+        Args:
+            hidden: Tensor, shape (batch_size, 1+2+...+seq_len, hidden_size)
+
+        Returns:
+            outputs: Tensor, shape (batch_size, num_relations, 1+2+...+seq_len, num_tags=3)
+        """
+        outputs = []
+        for fc in self.fc_layers:
+            outputs.append(fc(hidden))
+        outputs = torch.stack(outputs, dim=1)
+        return outputs
+
+
+class ConcatHandshaking(nn.Module):
+
+    def __init__(self, hidden_size, **kwargs):
+        super().__init__()
+        self.fc = nn.Linear(hidden_size * 2, hidden_size)
+
+    def forward(self, hidden, **kwargs):
+        """Handshaking.
+
+        Args:
+            hidden: Tensor, shape (batch_size, seq_len, hidden_size)
+
+        Returns:
+            handshaking_hiddens: Tensor, shape (batch_size, 1+2+...+seq_len, hidden_size)
+        """
+        seq_len = hidden.size()[1]
+        handshaking_hiddens = []
+        for i in range(seq_len):
+            _h = hidden[:, i, :]
+            repeat_hidden = _h[:, None, :].repeat(1, seq_len - i, 1)
+            visibl_hidden = hidden[:, i:, :]
+            shaking_hidden = torch.cat([repeat_hidden, visibl_hidden], dim=-1)
+            shaking_hidden = self.fc(shaking_hidden)
+            shaking_hidden = torch.tanh(shaking_hidden)
+            handshaking_hiddens.append(shaking_hidden)
+        handshaking_hiddens = torch.cat(handshaking_hiddens, dim=1)
+        return handshaking_hiddens
+
+
+class TPLinker(nn.Module):
+
+    def __init__(self, hidden_size, num_relations, max_positions=512, add_dist_embedding=False, **kwargs):
+        super().__init__()
+        self.handshaking = ConcatHandshaking(hidden_size)
+        self.h2t_proj = nn.Linear(hidden_size, 2)
+        self.h2h_proj = TaggingProjector(hidden_size, num_relations, name='H2HProjector')
+        self.t2t_proj = TaggingProjector(hidden_size, num_relations, name='T2TProjector')
+        self.add_dist_embedding = add_dist_embedding
+        if self.add_dist_embedding:
+            self.distance_embedding = DistanceEmbedding(max_positions, embedding_size=hidden_size)
+
+    def forward(self, hidden, **kwargs):
+        """TPLinker model forward pass.
+
+        Args:
+            hidden: Tensor, output of BERT or BiLSTM, shape (batch_size, seq_len, hidden_size)
+
+        Returns:
+            h2t_hidden: Tensor, shape (batch_size, 1+2+...+seq_len, 2),
+                logits for entity recognization
+            h2h_hidden: Tensor, shape (batch_size, num_relations, 1+2+...+seq_len, 3),
+                logits for relation recognization
+            t2t_hidden: Tensor, shape (batch_size, num_relations, 1+2+...+seq_len, 3),
+                logits for relation recognization
+        """
+        handshaking_hidden = self.handshaking(hidden)
+        h2t_hidden, rel_hidden = handshaking_hidden, handshaking_hidden
+        if self.add_dist_embedding:
+            h2t_hidden += self.distance_embedding(hidden)
+            rel_hidden += self.distance_embedding(hidden)
+        h2t_hidden = self.h2t_proj(h2t_hidden)
+        h2h_hidden = self.h2h_proj(rel_hidden)
+        t2t_hidden = self.t2t_proj(rel_hidden)
+        return h2t_hidden, h2h_hidden, t2t_hidden
---
+++ tplinker_pytorch/models_torch.py
@@ -0,0 +1,83 @@
+import logging
+
+import torch
+import torch.nn as nn
+from tokenizers import BertWordPieceTokenizer
+from transformers import BertModel
+
+from .layers_torch import GloveEmbedding, TPLinker
+
+
+class TPLinkerBert(nn.Module):
+
+    def __init__(self, bert_model_path, num_relations, add_dist_embedding=False, **kwargs):
+        super().__init__()
+        self.bert = BertModel.from_pretrained(bert_model_path)
+        self.tplinker = TPLinker(
+            hidden_size=self.bert.config.hidden_size,
+            num_relations=num_relations,
+            add_dist_embedding=add_dist_embedding,
+            max_positions=512)
+
+    def forward(self, inputs, **kwargs):
+        sequence_output = self.bert(inputs)[0]
+        h2t_outputs, h2h_outputs, t2t_outputs = self.tplinker(sequence_output)
+        return h2t_outputs, h2h_outputs, t2t_outputs
+
+
+class TPLinkerBiLSTM(nn.Module):
+
+    def __init__(self,
+                 num_relations,
+                 encoder_hidden_size,
+                 deocder_hidden_size,
+                 embedding_dropout_rate=0.1,
+                 lstm_dropout_rate=0.1,
+                 add_dist_embedding=False,
+                 max_positions=512,
+                 **kwargs):
+        super().__init__()
+        self.embedding = self._build_embedding(**kwargs)
+        self.embedding_dropout = nn.Dropout(embedding_dropout_rate)
+        self.encoder = nn.LSTM(
+            kwargs['embedding_size'],
+            encoder_hidden_size//2,
+            num_layers=1,
+            bidirectional=True,
+            batch_first=True)
+        self.decoder = nn.LSTM(
+            kwargs['embedding_size'],
+            deocder_hidden_size//2,
+            num_layers=1,
+            bidirectional=True,
+            batch_first=True)
+        self.lstm_dropout = nn.Dropout(lstm_dropout_rate)
+        self.tplinker = TPLinker(
+            hidden_size=deocder_hidden_size,
+            num_relations=num_relations,
+            max_positions=max_positions,
+            add_dist_embedding=add_dist_embedding,
+            **kwargs)
+
+    def _build_embedding(self, **kwargs):
+        pretrained_embedding_path = kwargs.get('pretrained_embedding_path', None)
+        vocab = kwargs.get('vocab', None)
+        embedding_size = kwargs.get('embedding_size', None)
+        assert embedding_size, "embedding_size must be provided."
+        if pretrained_embedding_path and vocab and embedding_size:
+            logging.info('Load pretrained embedding...')
+            embedding = GloveEmbedding(pretrained_embedding_path, vocab=vocab, embedding_size=embedding_size)
+            return embedding
+        vocab_size = kwargs.get('vocab_size', len(vocab) if vocab else None)
+        if vocab_size and embedding_size:
+            logging.info('Build embedding matrix...')
+            embedding = nn.Embedding(vocab_size, embedding_size)
+            return embedding
+        raise ValueError('Not enough params to build emebdding layer.')
+
+    def forward(self, input_ids, **kwargs):
+        embedding = self.embedding_dropout(self.embedding(input_ids))
+        lstm_outputs = self.lstm_dropout(self.encoder(embedding))
+        lstm_outputs = self.lstm_dropout(self.decoder(lstm_outputs))
+        h2t_outputs, h2h_outputs, t2t_outputs = self.tplinker(lstm_outputs)
+        return h2t_outputs, h2h_outputs, t2t_outputs
