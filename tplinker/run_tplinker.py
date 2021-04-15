d7bf75e8f8eb98bdede56d187d882758fb630068
luozhouyang <zhouyang.luo@gmail.com>
2021-04-15 15:03:42 +0800

    Update train step

--- tplinker/run_tplinker.py
+++ tplinker/run_tplinker.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 
 import pytorch_lightning as pl
@@ -10,6 +11,7 @@ from pytorch_lightning.callbacks import (Callback, EarlyStopping,
 from .dataset import TPLinkerBertDataset
 from .metrics import F1, Precision, Recall, SampleAccuracy
 from .models_torch import TPLinkerBert
+from .tagging_scheme import HandshakingTaggingDecoder, TagMapping
 
 
 def _compute_loss(y_pred, y_true):
@@ -46,15 +48,21 @@ class ONNXModelExport(Callback):
 
 class TPLinkerLightning(pl.LightningModule):
 
-    def __init__(self, model, **kwargs):
+    def __init__(self, model, rel2id_path, max_sequence_length=100, **kwargs):
         super().__init__(**kwargs)
         self.model = model
         self.h2t_acc = SampleAccuracy()
         self.h2h_acc = SampleAccuracy()
         self.t2t_acc = SampleAccuracy()
-        self.train_precision = Precision('data/tplinker/bert/rel2id.json', max_sequence_length=100)
-        self.train_recll = Recall('data/tplinker/bert/rel2id.json', max_sequence_length=100)
-        self.train_f1 = F1('data/tplinker/bert/rel2id.json', max_sequence_length=100)
+        self.train_precision = Precision()
+        self.train_recll = Recall()
+        self.train_f1 = F1()
+
+        with open(rel2id_path, mode='rt', encoding='utf-8') as fin:
+            rel2id = json.load(fin)
+        tag_mapping = TagMapping(rel2id)
+        self.decoder = HandshakingTaggingDecoder(tag_mapping)
+        self.max_sequence_length = max_sequence_length
 
     def forward(self, input_ids, attention_mask, token_type_ids, **kwargs):
         h2t, h2h, t2t = self.model(input_ids, attention_mask, token_type_ids)
@@ -62,10 +70,10 @@ class TPLinkerLightning(pl.LightningModule):
 
     def train_dataloader(self):
         train_dataset = TPLinkerBertDataset(
-            input_files=['data/tplinker/bert/valid_data.jsonl'],
+            input_files=['data/tplinker/bert/train_data.jsonl'],
             pretrained_bert_path='data/bert-base-cased',
             rel2id_path='data/tplinker/bert/rel2id.json')
-        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
         return train_dataloader
 
     def val_dataloader(self):
@@ -83,21 +91,36 @@ class TPLinkerLightning(pl.LightningModule):
         h2h_loss = _compute_loss(h2h_pred, batch['h2h'])
         t2t_loss = _compute_loss(t2t_pred, batch['t2t'])
         total_loss = 0.333 * h2t_loss + 0.333 * h2h_loss + 0.333 * t2t_loss
-        # logs = {'h2t_loss': h2t_loss, 'h2h_loss': h2h_loss, 't2t_loss': t2t_loss, 'loss': total_loss}
-        logs = {
-            'loss': total_loss,
-        }
-        # TODO: Fixed metrics
-        if self.trainer.current_epoch > 1:
-            preds = {'h2t': h2t_pred, 'h2h': h2h_pred, 't2t': t2t_pred}
-            target = {'example': batch['example']}
+        logs = {'h2t_loss': h2t_loss, 'h2h_loss': h2h_loss, 't2t_loss': t2t_loss}
+        logs.update({
+            'h2t_acc': self.h2t_acc(h2t_pred, batch['h2t']),
+            'h2h_acc': self.h2h_acc(h2h_pred, batch['h2h']),
+            't2t_acc': self.t2t_acc(t2t_pred, batch['t2t']),
+        })
+
+        # decoding predictions takes long time in early epochs, skip to decrease training time
+        if self.trainer.current_epoch > 0:
+            examples = [json.loads(e) for e in batch['example']]
+            pred_relations = self.decoder.batch_decode(
+                examples,
+                h2t_pred, h2h_pred, t2t_pred,
+                max_sequence_length=self.max_sequence_length)
+            # print('num of pred relations: {}'.format([len(x) for x in pred_relations]))
+            gold_relations = [e['relation_list'] for e in examples]
+            # TODO: Fixed metrics
             logs.update({
-                'precision': self.train_precision(preds, target),
-                'recall': self.train_recll(preds, target),
-                'f1': self.train_f1(preds, target)
+                'precision': self.train_precision(pred_relations, gold_relations),
+                'recall': self.train_recll(pred_relations, gold_relations),
+                'f1': self.train_f1(pred_relations, gold_relations),
             })
         self.log_dict(logs, prog_bar=True, on_step=True, on_epoch=False)
         return total_loss
+
+    def training_epoch_end(self, outputs):
+        print('correct: {}, prednum: {}, goldnum: {}'.format(
+            self.train_precision.correct,
+            self.train_precision.prednum,
+            self.train_precision.goldnum))
 
     def validation_step(self, batch, index, **kwargs):
         input_ids, attn_mask, type_ids = batch['input_ids'], batch['attention_mask'], batch['token_type_ids']
@@ -152,14 +175,17 @@ if __name__ == "__main__":
     parser.add_argument('--model_path', default='data/model/tplinker-bert/v0')
     parser.add_argument('--save_top_k', default=5)
     parser.add_argument('--max_epochs', default=10)
+    parser.add_argument('--max_sequence_length', default=100)
 
     args, _ = parser.parse_known_args()
 
-    module = TPLinkerLightning(create_bert_model(
-        pretrained_bert_path=args.pretrained_bert_path,
-        num_relations=args.num_relations,
-        add_distance_embedding=args.add_distance_embedding,
-    ))
+    module = TPLinkerLightning(
+        model=create_bert_model(
+            pretrained_bert_path=args.pretrained_bert_path,
+            num_relations=args.num_relations,
+            add_distance_embedding=args.add_distance_embedding),
+        rel2id_path='data/tplinker/bert/rel2id.json',
+        max_sequence_length=args.max_sequence_length)
     trainer = create_trainer(
         model_path=args.model_path,
         gpus=args.gpus,
